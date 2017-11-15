469262c8acda4fa440191e2598208021605a29e7
Dmitriy Kaminskiy <dimitriy.kaminskiy@nextiva.com>
2017-11-15 12:20:39 +0200

    fix

--- app/selectors.js
+++ app/selectors.js
@@ -4,7 +4,11 @@ import find from 'lodash/find';
 export const getRouteComponent = route => get(route, 'props.component');
 
 export const getSelectedTabIndex = (tabsData, pathname) => {
-  const lastFragment = pathname.split('/').pop();
+  const pathnameFragments = pathname.split('/').filter(Boolean);
+
+  const lastFragment = pathnameFragments.pop();
+
+  if (!lastFragment) return 0;
 
   const searchResult = find(tabsData, tabData => tabData.id === lastFragment);
 
